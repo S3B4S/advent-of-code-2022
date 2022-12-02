f70b99d4c0ba5fe9880249aac0718576e741969b
Kevin van der Werff <kevinsebastianate@hotmail.com>
2022-12-02 11:31:16 +0100

    solve part 2

--- day-02/main.hs
+++ day-02/main.hs
@@ -27,11 +27,17 @@ shapeSelectionPoints Scissors = 3
 
 data Outcome = Loss | Draw | Win deriving (Show)
 
+encodeOutcome :: Char -> Outcome
+encodeOutcome 'X' = Loss
+encodeOutcome 'Y' = Draw
+encodeOutcome 'Z' = Win
+
 pointsOutcome :: Outcome -> Integer
 pointsOutcome Loss = 0
 pointsOutcome Draw = 3
 pointsOutcome Win = 6
 
+-- Part 1
 -- (OpponentMove, MyMove)
 rules :: (Hand, Hand) -> Outcome
 rules (Rock, Paper) = Win
@@ -45,11 +51,33 @@ rules _ = Draw -- all the other cases are draws
 calculateBattle :: (OpponentMove, MyMove) -> Integer
 calculateBattle battle@(oppMove, myMove) = (shapeSelectionPoints myMove) + (pointsOutcome $ rules battle)
 
+-- Part 2
+-- opponent move -> desired outcome -> my move
+pickMove :: Hand -> Outcome -> Hand
+pickMove hand Draw = hand
+pickMove Rock Win = Paper
+pickMove Rock Loss = Scissors
+pickMove Paper Win = Scissors
+pickMove Paper Loss = Rock
+pickMove Scissors Win = Rock
+pickMove Scissors Loss = Paper
+
+calculateBattle2 :: (OpponentMove, Outcome) -> Integer
+calculateBattle2 battle@(oppMove, desiredOutcome) = (shapeSelectionPoints myMove) + (pointsOutcome desiredOutcome)
+  where myMove = pickMove oppMove desiredOutcome
+
+-- Parsing
 splitOn :: String -> T.Text -> [T.Text]
 splitOn = T.splitOn . T.pack
 
-encodeLine :: String -> (OpponentMove, MyMove)
-encodeLine line = (encodeOpponentMove move, encodeMyMove outcome)
+encodeLinePart1 :: String -> (OpponentMove, MyMove)
+encodeLinePart1 line = (encodeOpponentMove move, encodeMyMove outcome)
+  where
+    move = head line
+    outcome = last line
+
+encodeLinePart2 :: String -> (OpponentMove, Outcome)
+encodeLinePart2 line = (encodeOpponentMove move, encodeOutcome outcome)
   where
     move = head line
     outcome = last line
@@ -61,4 +89,7 @@ main = do
   let inputParsed = (splitOn "\n") . T.strip $ input
 
   -- part 1
-  print $ sum $ map (calculateBattle . encodeLine . T.unpack) inputParsed
+  print $ sum $ map (calculateBattle . encodeLinePart1 . T.unpack) inputParsed -- 12535
+
+  -- part 2
+  print $ sum $ map (calculateBattle2 . encodeLinePart2 . T.unpack) inputParsed -- 15457
