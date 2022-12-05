8d7e0973e6c7a1d3fa6f914ccd018a2e88d5d051
Kevin van der Werff <kevinsebastianate@hotmail.com>
2022-12-05 09:04:05 +0100

    solve day 5 - part 1

---
+++ day-05/index.ts
@@ -0,0 +1,92 @@
+import fs from 'fs';
+import { compose } from '../utilts';
+
+type Containers = (string | undefined)[][]
+
+const transpose = <T>(matrix: T[][]) => {
+  const res = Array.from({ length: matrix[0].length }).map(() => [])
+  
+  for (let rowIndex = matrix.length - 1; rowIndex >= 0; rowIndex--) {
+    const row = matrix[rowIndex];
+    for (let elIndex = 0; elIndex < row.length; elIndex++) {
+      const element = row[elIndex];
+      res[elIndex].push(element)
+    }
+  }
+
+  return res
+}
+
+/**
+ * MUTATING!
+ * @param containers 
+ * @param param1 
+ */
+const moveContainer = (containers: Containers, [amount, from, to]: [number, number, number]) => {
+  for (let i = 0; i < amount; i++) {
+    const el = containers[from].pop()
+    containers[to].push(el)
+  }
+}
+
+const takeWhile = <T>(predicate: (e: T) => boolean, list: T[]) => {
+  const res = []
+  for (const el of list) {
+    if (predicate(el)) {
+      res.push(el)
+    } else {
+      return res
+    }
+  }
+  return res
+}
+
+const dropWhile = <T>(predicate: (e: T) => boolean, list: T[]) => {
+  const index = list.findIndex(e => !predicate(e))
+  return list.slice(index)
+}
+
+const instructionLine = (line: string) => line.trim().startsWith('move')
+const containerLine = (line: string) => line.trim().startsWith('[')
+const columnLine = (line: string) => line.trim().startsWith('1')
+
+// @TODO use regex
+const parseInstruction = (line: string) => {
+  const triple = line.split(' ').filter(n => !['move', 'from', 'to'].includes(n)).map(Number) as [number, number, number]
+  return [triple[0], triple[1] - 1, triple[2] - 1]  as [number, number, number]
+}
+const parseContainerLine = (line: string): string[] => {
+  if (line.length <= 3) return [line]
+  return [line.slice(0, 3)].concat(parseContainerLine(line.slice(4)))
+}
+
+const buildContainers = (parsedLines: string[]) => parsedLines.map(l => {
+  const res = l.trim()
+  if (res === '') return res
+  
+  return res.slice(1,2)
+})
+
+const input = fs.readFileSync(__dirname + '/../../day-05/input.txt', 'utf-8')
+const containers = 
+  transpose(takeWhile(l => !columnLine(l), input.trimEnd().split('\n'))
+    .map(compose(
+      buildContainers,
+      parseContainerLine
+  ))).map(containers => containers.filter(c => c !== ''))
+  
+console.log(containers)
+  
+const instructions = dropWhile(l => !instructionLine(l), input.trimEnd().split('\n'))
+
+for (const instruction of instructions) {
+  // console.log('======')
+  // console.log(parseInstruction(instruction))
+  moveContainer(containers, parseInstruction(instruction))
+}
+// moveContainer(containers, [6, 8, 2])
+
+console.log(containers)
+
+export const part1 = containers.map(c => c[c.length - 1]).join('')
+export const part2 = ''
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import * as Day3 from './day-03'
 import * as Day4 from './day-04'
+import * as Day5 from './day-05'
 
 const printDay = (day: number, part1: any, part2: any, endsWithNewLine: boolean = true) => {
   console.log("-- DAY " + String(day).padStart(2, '0') + " --")
@@ -11,6 +12,7 @@ const printDay = (day: number, part1: any, part2: any, endsWithNewLine: boolean 
 const print: [number, any, any][] = [
   [3, Day3.part1 /* 8252 */, Day3.part2 /* 2828 */],
   [4, Day4.part1 /* 466 */, Day4.part2 /* 865 */],
+  [5, Day5.part1 /* X */, Day5.part2 /* Y */],
 ]
 
 print.forEach(([d, one, two], i) => printDay(d, one, two, i !== print.length - 1))
