03219d7da110e79cd3d41a3a1628863eb224a5f6
Kevin van der Werff <Kevinsebastianate@hotmail.com>
2022-12-05 11:10:39 +0100

    improvements

--- day-05/index.ts
+++ day-05/index.ts
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import { compose } from '../utilts';
 
-type Containers = string[][]
-
+/* Utils, to be added to the folder later */
 const transpose = <T>(matrix: T[][]) => {
   const res = Array.from({ length: matrix[0].length }).map(() => [])
   
@@ -15,24 +14,6 @@ const transpose = <T>(matrix: T[][]) => {
   }
 
   return res
-}
-
-/** MUTATING! */
-const moveContainersOneByOne = (containers: Containers, [amount, from, to]: [number, number, number]) => {
-  for (let i = 0; i < amount; i++) {
-    const el = containers[from].pop()
-    containers[to].push(el)
-  }
-}
-
-/** MUTATING! */
-const moveContainersGrouped = (containers: Containers, [amount, from, to]: [number, number, number]) => {
-  const toMove = []
-  for (let i = 0; i < amount; i++) {
-    const el = containers[from].pop()
-    toMove.push(el)
-  }
-  containers[to] = containers[to].concat(toMove.reverse())
 }
 
 const takeWhile = <T>(predicate: (e: T) => boolean, list: T[]) => {
@@ -52,21 +33,46 @@ const dropWhile = <T>(predicate: (e: T) => boolean, list: T[]) => {
   return list.slice(index)
 }
 
+type Containers = string[][]
+
+/** MUTATING! */
+const moveContainersOneByOne = (containers: Containers, [amount, from, to]: [number, number, number]) => {
+  for (let i = 0; i < amount; i++) {
+    const el = containers[from].pop()
+    containers[to].push(el)
+  }
+}
+
+/** MUTATING! */
+const moveContainersGrouped = (containers: Containers, [amount, from, to]: [number, number, number]) => {
+  const toMove = []
+  for (let i = 0; i < amount; i++) {
+    const el = containers[from].pop()
+    toMove.push(el)
+  }
+  containers[to] = containers[to].concat(toMove.reverse())
+}
+
 const instructionLine = (line: string) => line.trim().startsWith('move')
 const containerLine = (line: string) => line.trim().startsWith('[')
 const columnLine = (line: string) => line.trim().startsWith('1')
 
-// @TODO use regex
-const parseInstruction = (line: string) => {
-  const triple = line.split(' ').filter(n => !['move', 'from', 'to'].includes(n)).map(Number) as [number, number, number]
-  return [triple[0], triple[1] - 1, triple[2] - 1]  as [number, number, number]
+type Instruction = [
+  number, // Amount of containers to move
+  number, // From this column
+  number, // to this column
+]
+const parseInstruction = (line: string): Instruction => {
+  // @TODO Could use regex instead
+  const triple = line.split(' ').filter(n => !['move', 'from', 'to'].includes(n)).map(Number)
+  return [triple[0], triple[1] - 1, triple[2] - 1]
 }
 const parseContainerLine = (line: string): string[] => {
   if (line.length <= 3) return [line]
   return [line.slice(0, 3)].concat(parseContainerLine(line.slice(4)))
 }
 
-const buildContainers = (parsedLines: string[]) => parsedLines.map(l => {
+const unpackContainers = (parsedLines: string[]) => parsedLines.map(l => {
   const res = l.trim()
   if (res === '') return res
   
@@ -76,7 +82,7 @@ const buildContainers = (parsedLines: string[]) => parsedLines.map(l => {
 const input = fs.readFileSync(__dirname + '/../../day-05/input.txt', 'utf-8').trimEnd().split('\n')
 const containersParsed = takeWhile(l => !columnLine(l), input)
   .map(compose(
-    buildContainers,
+    unpackContainers,
     parseContainerLine
   ))
 const instructions = dropWhile(l => !instructionLine(l), input)
