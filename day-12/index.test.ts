7379739569cd6316212cde183843ef2ead996a08
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2022-12-12 12:34:54 +0100

    recursive dfs appraoch day 12 part 1

--- day-12/index.test.ts
+++ day-12/index.test.ts
@@ -1,17 +1,73 @@
 import { assertEquals } from "../deps.ts"
-import { solvePart1, solvePart2 } from "./index.ts"
+import { isAtMostOneElevated, solvePart1, solvePart2 } from "./index.ts"
 
 const exampleInput = `
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+`
 
+const loop = `
+Saaaabuv
+aaaaactw
+aaaaadsx
+jihgfery
+klmnopqE
 `
 
 const fileInput = Deno.readTextFileSync('./day-12/input.txt')
 
-Deno.test("Day 12 - Part 1 - Example input", () => {
-  assertEquals(0, solvePart1(exampleInput))
+Deno.test("Day 12 - Is at most one elevated 1", () => {
+  assertEquals(true, isAtMostOneElevated("b", "c"))
 })
 
-Deno.test("Day 12 - Part 1 - File input", () => {
+Deno.test("Day 12 - Is at most one elevated 2", () => {
+  assertEquals(true, isAtMostOneElevated("b", "b"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 3", () => {
+  assertEquals(true, isAtMostOneElevated("b", "a"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 4", () => {
+  assertEquals(true, isAtMostOneElevated("S", "a"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 5", () => {
+  assertEquals(true, isAtMostOneElevated("S", "b"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 6", () => {
+  assertEquals(false, isAtMostOneElevated("S", "c"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 7", () => {
+  assertEquals(true, isAtMostOneElevated("y", "E"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 8", () => {
+  assertEquals(false, isAtMostOneElevated("x", "E"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 9", () => {
+  assertEquals(false, isAtMostOneElevated("S", "E"))
+})
+
+Deno.test("Day 12 - Is at most one elevated 10", () => {
+  assertEquals(true, isAtMostOneElevated("h", "b"))
+})
+
+Deno.test("Day 12 - Part 1 - Example input", () => {
+  assertEquals(31, solvePart1(exampleInput))
+})
+
+Deno.test("Day 12 - Part 1 - Example input with loop of a's", () => {
+  assertEquals(29, solvePart1(loop))
+})
+
+Deno.test("Day 12 - Part 1 - File input", {only: true}, () => {
   assertEquals(0, solvePart1(fileInput))
 })
 
--- day-12/index.ts
+++ day-12/index.ts
@@ -1,8 +1,81 @@
-// deno-lint-ignore-file
-// @TODO Remove above line when starting on this day
+import { addLists, Coordinate } from 'utils'
+
+export const isAtMostOneElevated = (currentTile: string, targetTile: string) => {
+  if (currentTile === "S") {
+    currentTile = "a"
+  } else if (currentTile === "E") {
+    currentTile = "z"
+  }
+  
+  if (targetTile === "S") {
+    targetTile = "a"
+  } else if (targetTile === "E") {
+    targetTile = "z"
+  }
+  return targetTile.charCodeAt(0) - 1 <= currentTile.charCodeAt(0)
+}
+
+const Direction = {
+  North: [-1, 0],
+  East: [0, 1],
+  South: [1, 0],
+  West: [0, -1],
+}
+
+const deptFirstSearchRec = (grid: string[][], visited: Set<string>, currentLocation: Coordinate): number | undefined => {
+  // Base case
+  if (grid[currentLocation[0]][currentLocation[1]] === "E") {
+    return 0
+  }
+  
+  // console.log({ currentLocation, visited })
+
+  // Calculate possible locations
+  const candidates: Coordinate[] = Object.values(Direction)
+    .map(direction => addLists(direction, currentLocation) as Coordinate)
+    // .map(x => { console.log(x); return x})
+    .filter(candidate => 
+      // And candidate is not out of bounds
+      !(candidate[0] < 0 || candidate[0] > grid.length - 1 || candidate[1] < 0 || candidate[1] > grid[0].length - 1)
+      // And we have not visited this location before
+      && !visited.has(candidate.toString())
+      // We can jump to candidate
+      && isAtMostOneElevated(grid[currentLocation[0]][currentLocation[1]], grid[candidate[0]][candidate[1]])
+    )
+
+  // If there are no candidates, we're at a dead end
+  if (candidates.length === 0) {
+    return undefined
+  }
+  
+  const newVisited = new Set(visited)
+  newVisited.add(currentLocation.toString())
+  const results = candidates.flatMap(nextLocation => {
+    const res = deptFirstSearchRec(grid, newVisited, nextLocation)
+    if (res === undefined) {
+      return []
+    } else {
+      return 1 + res
+    }
+  })
+
+  // All the candidates had reached a dead end
+  if (results.length === 0) return undefined
+  
+  return Math.min(...results)
+}
+
 
 export const solvePart1 = (input: string) => {
-  return 0
+  let startLocation: Coordinate = [-1, -1]
+  const grid = input.trim().split('\n').map((line, rowIndex) => line.split('').map((cell, columnIndex) => {
+    if (cell === "S") startLocation = [rowIndex, columnIndex]
+    return cell
+  }))
+
+  const visited = new Set<string>()
+  const amountSteps = deptFirstSearchRec(grid, visited, startLocation)
+  return amountSteps
 }
 
 export const solvePart2 = (input: string) => {
