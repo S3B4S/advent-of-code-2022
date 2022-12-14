6ebb199e3bfc37b528d79e248229011715553ef4
Kevin van der Werff <kevinsebastianate@hotmail.com>
2022-12-14 17:06:08 +0100

    parse input

--- 14_regolith-reservoir/index.test.ts
+++ 14_regolith-reservoir/index.test.ts
@@ -2,12 +2,13 @@ import { assertEquals } from "deps"
 import { solvePart1, solvePart2 } from "./index.ts"
 
 const exampleInput = `
-
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
 `
 
 const fileInput = Deno.readTextFileSync('./14_regolith-reservoir/input.txt')
 
-Deno.test("Day 14 - Part 1 - Example input", () => {
+Deno.test("Day 14 - Part 1 - Example input", {only: true}, () => {
   assertEquals(0, solvePart1(exampleInput))
 })
 
--- 14_regolith-reservoir/index.ts
+++ 14_regolith-reservoir/index.ts
@@ -1,7 +1,31 @@
-// deno-lint-ignore-file
-// @TODO Remove above line when starting on this day
+import { parseNumbersWithDelimiter } from '../parsing.ts'
+import { monpar } from 'deps'
+import { Coordinate } from 'utils'
+const { liftAs, many, token, sentence, unpack } = monpar
+
+type Range = Coordinate[]
+
+export const parseCoordinate = parseNumbersWithDelimiter(',')
+
+export const parseLine = liftAs(
+  (numbers: number[][]) => (number: number[]) => numbers.concat([number]),
+  many(liftAs(
+    (numbers: number[]) => () => numbers,
+    parseCoordinate,
+    sentence(" -> ")
+  )),
+  token(parseCoordinate),
+)
+
+export const parseInput = liftAs<Range[]>(
+  (ranges: number[][][]) => ranges,
+  token(many(parseLine)),
+)
 
 export const solvePart1 = (input: string) => {
+  const ranges = unpack(parseInput)(input)!
+  ranges.forEach(r => console.log(r))
+
   return 0
 }
 
