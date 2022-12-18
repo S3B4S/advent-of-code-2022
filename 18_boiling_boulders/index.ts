15125bc8ada7eef82db892743f916ed2b01e57a5
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2022-12-18 18:50:01 +0100

    Day 18 - Part 1

--- 18_boiling_boulders/index.ts
+++ 18_boiling_boulders/index.ts
@@ -1,8 +1,46 @@
-// deno-lint-ignore-file
-// @TODO Remove above line when starting on this day
+import { lines } from '../utilts.ts'
+
+type Coordinate = [
+  number, // x
+  number, // y
+  number, // z
+]
+
+const checkAdjacent = (map: Record<string, boolean>, [x, y, z]: Coordinate) => {
+  let openAirCount = 6
+  
+  // Check x axis
+  if (map[[x + 1, y, z].toString()]) openAirCount--
+  if (map[[x - 1, y, z].toString()]) openAirCount--
+  
+  // Check y axis
+  if (map[[x, y + 1, z].toString()]) openAirCount--
+  if (map[[x, y - 1, z].toString()]) openAirCount--
+  
+  // Check z axis
+  if (map[[x, y, z + 1].toString()]) openAirCount--
+  if (map[[x, y, z - 1].toString()]) openAirCount--
+
+  return openAirCount
+}
 
 export const solvePart1 = (input: string) => {
-  return 0
+  const coordinates: Coordinate[] = lines(input).map(l => {
+    return l.split(',').map(c => Number(c)) as Coordinate
+  })
+  
+  const map: Record<string, boolean> = {}
+
+  coordinates.forEach(coordinate => [
+    map[coordinate.toString()] = true
+  ])
+
+  let openAirCount = 0
+  coordinates.forEach(coord => {
+    openAirCount += checkAdjacent(map, coord)
+  })
+
+  return openAirCount
 }
 
 export const solvePart2 = (input: string) => {
