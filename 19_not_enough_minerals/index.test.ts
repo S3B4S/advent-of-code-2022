28340e3f6994017493712ad2f2d225e0190d1d98
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2022-12-21 23:01:44 +0100

    Day 19 init + parse robot

--- 19_not_enough_minerals/index.test.ts
+++ 19_not_enough_minerals/index.test.ts
@@ -2,10 +2,11 @@ import { assertEquals } from "deps"
 import { solvePart1, solvePart2 } from "./index.ts"
 
 const exampleInput = `
-
+Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
 `
 
-const fileInput = Deno.readTextFileSync('./day-19/input.txt')
+const fileInput = Deno.readTextFileSync('./19_not_enough_minerals/input.txt')
 
 Deno.test("Day 19 - Part 1 - Example input", () => {
   assertEquals(0, solvePart1(exampleInput))
