007636cce6e837e3b5154833ce291319d27f11ac
Kevin van der Werff <kevinsebastianate@hotmail.com>
2022-12-31 18:58:25 +0100

    init day 25

--- 25_full_of_hot_air/index.test.ts
+++ 25_full_of_hot_air/index.test.ts
@@ -2,10 +2,22 @@ import { assertEquals } from "deps"
 import { solvePart1, solvePart2 } from "./index.ts"
 
 const exampleInput = `
-
+1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122
 `
 
-const fileInput = Deno.readTextFileSync('./day-25/input.txt')
+const fileInput = Deno.readTextFileSync('./25_full_of_hot_air/input.txt')
 
 Deno.test("Day 25 - Part 1 - Example input", () => {
   assertEquals(0, solvePart1(exampleInput))
